d51ec4ab7a17aa26a50d8001e17e9436bc0d46ac
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-22 18:20:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,18 +1,20 @@
-
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
   get '/friends' do
-    #write your code here
+    @friends = ["Emily Wilding Davidson", "harriet Tubman", "Joan of Arc", "Malala Yousafzai", "Sojourner Truth"]
+    
+    erb :friends
   end
 
 end
