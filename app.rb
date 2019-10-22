bebedb52a7657ea94bfef1727de52bd286fb8801
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-22 18:26:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/reverse' do
+    erb :reverse
+  end
+
   post '/reverse' do
     original_string = params["string"]
     @reversed_string = original_string.reverse
